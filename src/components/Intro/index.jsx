008164a6e534026a4e2bf4562b05e2c54b9e09e7
lauro <laurolyra@gmail.com>
2022-05-27 20:42:14 -0300

    intro component set

---
+++ src/components/Intro/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.scss';
+
+const Intro = () => (
+  <div className='intro-container'>
+    <h1>Então você quer aprender React...</h1>
+    <h2>Ótimo, mas vamos aprender algumas coisinhas de JavaScript antes?</h2>
+  </div>
+  );
+
+export default Intro;
