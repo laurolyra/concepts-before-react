20465dc92dcec477579629ca8cbb899434cbe793
lauro <laurolyra@gmail.com>
2022-05-27 22:33:02 -0300

    footer created

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import './App.css';
-import Intro from './components/Intro';
-import ConceptsArea from './components/ConceptsArea';
-import Concept from './components/Concept';
+import "./App.css";
+import Intro from "./components/Intro";
+import ConceptsArea from "./components/ConceptsArea";
+import Concept from "./components/Concept";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Intro/>
-      <ConceptsArea/>
-      <Concept title='Array.map' id="array-map" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map'/>
-      <Concept title='Array.filter' id="array-filter" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter'/>
-      <Concept title='Array.forEach' id="array-forEach" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/foreach'/>
-      <Concept title='Array.reduce' id="array-reduce" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce'/>
-      <Concept title='Sintaxe de Espalhamento' id="spread-operator" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Spread_syntax'/>
-      <Concept title='Parâmetro Rest' id="rest-parameter" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/rest_parameters'/>
-      <Concept title='Trabalhando com objetos' id="working-with-objects" link='https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Objects/Basics'/>
-      <Concept title='Atribuição via desestruturação' id="destructuring" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment'/>
-      <Concept title='Object.keys' id="object-keys" link='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys'/>
+      <Intro />
+      <ConceptsArea />
+      <Concept
+        title="Array.map"
+        id="array-map"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/map"
+      />
+      <Concept
+        title="Array.filter"
+        id="array-filter"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/filter"
+      />
+      <Concept
+        title="Array.forEach"
+        id="array-forEach"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/foreach"
+      />
+      <Concept
+        title="Array.reduce"
+        id="array-reduce"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce"
+      />
+      <Concept
+        title="Sintaxe de Espalhamento"
+        id="spread-operator"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Spread_syntax"
+      />
+      <Concept
+        title="Parâmetro Rest"
+        id="rest-parameter"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/rest_parameters"
+      />
+      <Concept
+        title="Trabalhando com objetos"
+        id="working-with-objects"
+        link="https://developer.mozilla.org/pt-BR/docs/Learn/JavaScript/Objects/Basics"
+      />
+      <Concept
+        title="Atribuição via desestruturação"
+        id="destructuring"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment"
+      />
+      <Concept
+        title="Object.keys"
+        id="object-keys"
+        link="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys"
+      />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.scss';
+
+const Footer = () => (
+  <div className='footer-container'>
+    <h1>Site desenvolvido por Lauro Lyra Aguiar</h1>
+    <div className="personal-links">
+      <a>github</a>
+      <a>linkedIn</a>
+      <a>twitter</a>
+      <a>discord</a>
+    </div>
+    <h4>Curtiu? me paga um cafezinho! Pix: f552a562-b73e-40f4-b8ce-6d8d6db242f1</h4>
+  </div>
+  );
+
+export default Footer;
